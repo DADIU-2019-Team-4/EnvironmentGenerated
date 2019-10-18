caf21e40a9d24d877b24e097dd1d21ba7335990f
MichaeliusDenModige <s143049@student.dtu.dk>
2019-10-18 13:06:00 +0200

    Added particle benchmarks

---
+++ Assets/Scripts/SwitchActivity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchActivity : MonoBehaviour
+{
+    public GameObject[] SwitchObjects;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnMouseDown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        foreach (var o in SwitchObjects)
+            o.SetActive(false);
+
+        index = (++index) % SwitchObjects.Length;
+
+        SwitchObjects[index].SetActive(true);
+    }
+}
