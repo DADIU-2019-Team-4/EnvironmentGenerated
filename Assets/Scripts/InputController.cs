be90bfebccf3b9ecef939396136e7b246e170282
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-10-21 16:19:16 +0200

    made first prototype where you can dash in 4 directions

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    private float minSwipeDistanceInPercentage = 0.10f;
+    private float verticalSwipeDistance;
+    private float horizontalSwipeDistance;
+
+    private readonly Vector3[] firstPosition = new Vector3[2];
+    private readonly Vector3[] lastPosition = new Vector3[2];
+    private bool hasSwiped;
+
+    private bool trackMouse;
+
+    private PlayerMovement playerMovement;
+    private float timer;
+
+    private void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void Start()
+    {
+        verticalSwipeDistance = Screen.height * minSwipeDistanceInPercentage;
+        horizontalSwipeDistance = Screen.width * minSwipeDistanceInPercentage;
+    }
+
+
+    /// <summary>
+    /// Update function.
+    /// </summary>
+    public void Update()
+    {
+        HandleInput();
+    }
+
+
+    /// <summary>
+    /// Handles the input.
+    /// </summary>
+    private void HandleInput()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        PcInput();
+#elif UNITY_ANDROID || UNITY_IOS
+        MobileInput();
+#endif
+    }
+
+
+    /// <summary>
+    /// Handles mobile input.
+    /// </summary>
+    private void MobileInput()
+    {
+        Touch[] touches = Input.touches;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (touches[i].phase == TouchPhase.Began)
+            {
+                firstPosition[i] = touches[i].position;
+                lastPosition[i] = touches[i].position;
+            }
+            else if (touches[i].phase == TouchPhase.Moved)
+            {
+                if (!hasSwiped)
+                {
+                    lastPosition[i] = touches[i].position;
+                    CheckSwipe(i);
+                }
+            }
+            else if (touches[i].phase == TouchPhase.Ended)
+            {
+                hasSwiped = false;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Handles pc input.
+    /// </summary>
+    private void PcInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            trackMouse = true;
+            firstPosition[0] = Input.mousePosition;
+            lastPosition[0] = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            timer += Time.deltaTime;
+            if (timer >= playerMovement.ChargeThreshold)
+            {
+                playerMovement.ChargeDash();
+                playerMovement.IsDashCharged = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            trackMouse = false;
+            timer = 0;
+            playerMovement.ResetDash();
+        }       
+
+        if (trackMouse)
+        {
+            lastPosition[0] = Input.mousePosition;
+            CheckSwipe(0);
+        }
+    }
+
+
+    /// <summary>
+    /// Checks how to player has swiped.
+    /// </summary>
+    private void CheckSwipe(int i)
+    {
+        Vector3 directionVector = lastPosition[i] - firstPosition[i];
+
+        if (SwipedLongEnough(directionVector)) return;
+
+        if (Mathf.Abs(directionVector.x) > Mathf.Abs(directionVector.y))
+        {
+            if (playerMovement.IsDashCharged)
+                playerMovement.StartDash(HorizontalSwipe(i));
+            else
+                playerMovement.StartMove(HorizontalSwipe(i));
+        }
+        else
+        {
+            if (playerMovement.IsDashCharged)
+                playerMovement.StartDash(VerticalSwipe(i));
+            else
+                playerMovement.StartMove(VerticalSwipe(i));
+        }
+
+        hasSwiped = true;
+    }
+
+
+    private bool SwipedLongEnough(Vector3 direction)
+    {
+        return (!(Math.Abs(direction.x) > horizontalSwipeDistance) &&
+            !(Math.Abs(direction.y) > verticalSwipeDistance));
+    }
+
+
+    private Vector3 HorizontalSwipe(int i)
+    {
+        if (lastPosition[i].x > firstPosition[i].x)
+            return Vector3.right;
+
+        return Vector3.left;
+    }
+
+
+    private Vector3 VerticalSwipe(int i)
+    {
+        if (lastPosition[i].y > firstPosition[i].y)
+            return Vector3.forward;
+
+        return Vector3.back;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int DashMultiplier = 2000;
+    public int MoveMultiplier = 200;
+    public float CoolDownValue = 0.3f;
+    public float ChargeThreshold = 0.5f;
+    public float DashDuration = 0.1f;
+    public float MoveDuration = 0.2f;
+
+    private Rigidbody rigidBody;
+    private Material material;
+    private Vector3 spawnPos;
+    private readonly int maxNegativeY = -5;
+    private bool isMoving;
+    private float colorValue = 1;
+
+    public bool IsDashCharged { get; set; }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        material = GetComponent<Renderer>().material;
+        spawnPos = rigidBody.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (rigidBody.position.y < maxNegativeY)
+        {
+            rigidBody.position = spawnPos;
+            rigidBody.velocity = Vector3.zero;
+        }
+    }
+
+    public void ChargeDash()
+    {
+        material.color = new Color(1, colorValue, colorValue, 1);
+        colorValue -= 0.01f;
+    }
+
+    public void StartDash(Vector3 dashDirection)
+    {
+        if (isMoving)
+            return;
+
+        isMoving = true;
+
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(dashDirection * DashMultiplier);
+
+        ResetDash();
+
+        StartCoroutine(StartCoolDown(DashDuration));
+    }
+
+    public void ResetDash()
+    {
+        material.SetColor("_Color", Color.white);
+        colorValue = 1f;
+        IsDashCharged = false;
+    }
+
+    public void StartMove(Vector3 moveDirection)
+    {
+        if (isMoving)
+            return;
+
+        isMoving = true;
+
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(moveDirection * MoveMultiplier);
+        StartCoroutine(StartCoolDown(MoveDuration));
+    }
+
+    IEnumerator StartCoolDown(float duration)
+    {
+        yield return  new WaitForSeconds(duration);
+        rigidBody.velocity = Vector3.zero;
+
+        yield return new WaitForSeconds(CoolDownValue);
+        isMoving = false;
+    }
+}
